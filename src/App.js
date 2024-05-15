801d712a29bf8ff695f36bd39a7c87ec0d78759a
low-q <alotaibi.fahad21@gmail.com>
2024-05-14 19:33:36 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       
     <section className="main-content">
         <h1 className="left-text">Fahad <br />Alotaibi</h1>
+        <h1>hhhee</h1>
         <div className="photo-container">
             <img src="grad.jpg" alt="Photo" />
             <img src="owilly.jpg" alt="Photo" />
