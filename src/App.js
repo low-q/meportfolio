576be8e09224da89f0fbbd606245c53e749b8a77
low-q <alotaibi.fahad21@gmail.com>
2024-05-21 20:34:03 -0500

    hear me out

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ function App() {
 
       <div className="about-me" id="scroll-target"> 
           <h1>Who am I?</h1>
-          <button className="down-arrow-button">
-              <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M6 9L12 15L18 9" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-          </button>
+          <button className="down-arrow-button" onClick={scrollToNextSection}>
+    <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 9L12 15L18 9" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+</button>
       </div>
 
       
